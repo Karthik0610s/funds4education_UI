9fe7efd1c77bcad209a3412854be878e60f8e705
Your Name <nanthinik30@gmail.com>
2025-11-19 18:14:10 +0530

    Scholarship application by student

--- src/api/scholarshipapplication/scholarshipapplication.js
+++ src/api/scholarshipapplication/scholarshipapplication.js
@@ -16,6 +16,22 @@ export const fetchScholarshipApplicationListReq = async (studentId) => {
     throw { error: true, data: "", message: "", errorMsg: error };
   }
 };
+export const fetchScholarshipApplicationListbyStudentReq
+ = async (studentId) => {
+  try {
+    debugger;
+    const res = await publicAxios.get(`${ApiKey.ScholarshipApplication}/byStudent/${studentId}`);
+
+    const _data = res.data;
+    return { error: false, data: _data, message: "", errorMsg: "" };
+  } catch (err) {
+    let error;
+    if (err.response) error = err.response.data.message || "Response error";
+    else if (err.request) error = "Request error";
+    else error = "Something went wrong please try again later";
+    throw { error: true, data: "", message: "", errorMsg: error };
+  }
+};
 export const fetchScholarshipApplicationByIdReq = async (id) => {
     try {
       debugger;
